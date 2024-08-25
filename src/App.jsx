abfe3bfcffda454974051cd0c89d565777726f7f
saemyyy <skusuma@sunrise.ch>
2024-08-25 14:53:29 +0200

    create base for project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WeatherChecker from "./components/WeatherChecker";
+import "./components/WeatherChecker.css";
+
+const App = () => {
+  return (
+    <div>
+      <WeatherChecker />
+    </div>
+  );
+};
+
+export default App;
