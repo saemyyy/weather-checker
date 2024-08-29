2237bb2359cc8db78d8207e8ab8fbb55031e3728
saemyyy <skusuma@sunrise.ch>
2024-08-29 23:30:38 +0200

    integrate functions of api. key must not be openly available

--- src/components/WeatherChecker.jsx
+++ src/components/WeatherChecker.jsx
@@ -2,25 +2,79 @@ import sunny from "../assets/images/sunny.png";
 import cloudy from "../assets/images/cloudy.png";
 import rainy from "../assets/images/rainy.png";
 import snowy from "../assets/images/snowy.png";
+import { useState } from "react";
 
 const WeatherChecker = () => {
+  const [data, setData] = useState([]);
+  const [location, setLocation] = useState("");
+  const api_key = "does not work without api key";
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const search = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${api_key}`;
+    const res = await fetch(url);
+    const searchData = await res.json();
+    console.log(searchData);
+    setData(searchData);
+    setLocation("");
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  const weatherImages = {
+    Clear: sunny,
+    Clouds: cloudy,
+    Rain: rainy,
+    Snow: snowy,
+    Haze: cloudy,
+    Mist: cloudy,
+  };
+
+  const weatherImage = data.weather
+    ? weatherImages[data.weather[0].main]
+    : null;
+
   return (
     <div className="container">
       <div className="weather-app">
         <div className="search">
           <div className="search-top">
             <i className="fa-solid fa-location-dot"></i>
-            <div className="location">London</div>
+            <div className="location">
+              {data.name ? data.name : "City"},
+              {data.sys && data.sys.country ? data.sys.country : "Country"}
+            </div>
           </div>
           <div className="search-bar">
-            <input type="text" placeholder="Location" />{" "}
-            <i className="fa-solid fa-magnifying-glass"></i>
+            <input
+              type="text"
+              placeholder="Location"
+              value={location}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />{" "}
+            <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
           </div>
         </div>
         <div className="weather">
-          <img src={sunny} alt="sunny" />
-          <div className="weather-type">Clear</div>
-          <div className="temp">20°</div>
+          <img
+            src={weatherImage}
+            alt="Weather"
+            style={{ display: weatherImage ? "block" : "none" }}
+          />
+          <div className="weather-type">
+            {data.weather ? data.weather.main : null}
+          </div>
+          <div className="temp">
+            {data.main ? Math.round(data.main.temp) + "°" : "0°"}
+          </div>
         </div>
         <div className="weather-date">
           <p>Fri, 3 May</p>
@@ -29,12 +83,18 @@ const WeatherChecker = () => {
           <div className="humidity">
             <div className="data-name">Humidity</div>
             <i className="fa-solid fa-droplet"></i>
-            <div className="data">35%</div>
+            <div className="data">
+              {data.main ? data.main.humidity + "%" : "0%"}
+            </div>
           </div>
           <div className="wind">
-            <div className="data-name">Humidity</div>
+            <div className="data-name">Wind Speed</div>
             <i className="fa-solid fa-wind"></i>
-            <div className="data">3km/h</div>
+            <div className="data">
+              {data.wind
+                ? Math.round(data.wind.speed * 3.6) + "km/h"
+                : "0 km/h"}
+            </div>
           </div>
         </div>
       </div>
